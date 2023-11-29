8996484ec47e2c8f6f79a13309f82e275359cf3f
Mohammad-Shahid-07 <112233075+Mohammad-Shahid-07@users.noreply.github.com>
2023-11-29 10:12:04 +0530

    made the og dynamic image routes public

--- middleware.tsx
+++ middleware.tsx
@@ -1,29 +1,27 @@
 import { authMiddleware } from "@clerk/nextjs";
- 
+
 // This example protects all routes including api/trpc routes
 // Please edit this to allow other routes to be public as needed.
 // See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your Middleware
 export default authMiddleware({
-    publicRoutes: [
-        '/',
-        '/api/webhook',
-        '/question/:slug/:id',
-        '/tags',
-        '/tags/:tag',
-        '/profile/:username',
-        '/community',
-        '/community/:id',
-        '/jobs'
-        
-    ],
-    ignoredRoutes:[
-      '/api/webhook',
-      '/api/chatbot',
-      
-    ]
+  publicRoutes: [
+    "/",
+    "/api/webhook",
+    "/question/:slug/:id",
+    "/question/:slug/:id/opengraph-image-:imageId",
+
+    "/tags",
+    "/tags/:tag",
+    "/tags/:tag/opengraph-image-:imageId",
+    "/profile/:username",
+    "/profile/:username/opengraph-image-:imageId",
+    "/community",
+    "/community/:id",
+    "/jobs",
+  ],
+  ignoredRoutes: ["/api/webhook", "/api/chatbot"],
 });
- 
+
 export const config = {
-  matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
+  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
 };
- 
